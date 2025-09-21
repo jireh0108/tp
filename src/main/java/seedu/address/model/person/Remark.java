b6a3e3897e6187ef9edd6bdb73d5ace62e56506d
Jireh <tohjireh9@gmail.com>
2025-09-21 21:37:24 +0800

    Fix code quality and tests

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -8,7 +8,11 @@ import static java.util.Objects.requireNonNull;
  */
 public class Remark {
     public final String value;
-
+    /**
+     * Constructs a {@code Remark}.
+     *
+     * @param remark A remark made about the person.
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
